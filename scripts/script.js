0e84ad9df127fb407a0ae87bd527ca302ef3ab27
UserMe <you@example.com>
2019-10-10 09:36:27 +0300

    On branch master  Changes to be committed: 	modified:   scripts/script.js 	modified:   specver/js/specver.js

--- scripts/script.js
+++ scripts/script.js
@@ -34,7 +34,7 @@ let carousel = document.querySelector('.carousel');
       li.style.position = 'relative';
       li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
       i++;
-    }
+    };
 
  
     let width = 235; // ширина картинки
