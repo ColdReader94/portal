605b50d62c592c60cb845aeda257ab2bbe97528f
UserMe <you@example.com>
2019-10-10 11:26:03 +0300

    On branch master  Changes to be committed: 	modified:   specver/js/specver.js

--- specver/js/specver.js
+++ specver/js/specver.js
@@ -4,24 +4,9 @@ function loadform(){
   $( "<button style='float:right'  onclick='$.removeCookie(\"openspec\", { path: \"/\" });location.reload();'>Закрыть версию для слабовидящих</button>" ).insertBefore( "body" );
 $( "body" ).before( "<iframe width='100%' height='110'  style='border: none' src='specver/spec.html'></iframe>" ); 
 
-};   
-//
-$('.spec').click(function (e) {
+};
 
-if (!$.cookie('openspec'))
-{
-$.cookie('openspec',1, { path: '/' });
-$("body *").css("background", "#fff");
-$("body *").css("color", "#000");
-$("img").css("-webkit-filter", "grayscale(100%)");
-loadform();
-}});
-
-if ($.cookie('openspec')==1)
-loadform();
-
-
-
+function loadmysets(){
 //Добавлено:
 //    спрятать карусель
     let carouselOffBlock = document.querySelector('.row-two .grid-item-left');
@@ -45,6 +30,23 @@ loadform();
     
     let searchForm = document.querySelector('.search-form');
     searchForm.classList.add('close');
+};
 
+
+//
+$('.spec').click(function (e) {
+
+if (!$.cookie('openspec'))
+{
+$.cookie('openspec',1, { path: '/' });
+$("body *").css("background", "#fff");
+$("body *").css("color", "#000");
+$("img").css("-webkit-filter", "grayscale(100%)");
+loadform();
+loadmysets();
+}});
+
+if ($.cookie('openspec')==1)
+loadform();
 });
 
