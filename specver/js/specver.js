8eba2d75e633003653e73a4a07d766f1f83214a6
UserMe <you@example.com>
2019-10-11 12:01:00 +0300

    Changes to be committed: 	modified:   specver/js/specver.js

--- specver/js/specver.js
+++ specver/js/specver.js
@@ -7,14 +7,8 @@ $( "body" ).before( "<iframe width='100%' height='110'  style='border: none' src
  
 $('.spec').click(function (e) {
 
-if (!$.cookie('openspec'))
-{
-$.cookie('openspec',1, { path: '/' });
-$("body *").css("background", "#fff");
-$("body *").css("color", "#000");
-$("img").css("-webkit-filter", "grayscale(100%)");
-loadform();
 
+function loadstyles() {
 //Добавлено:
 //    спрятать карусель, изображения блока ссылок, поиск, кнопку спецверсии
 $(".row-two .grid-item-left, .links a img, .search-form, .header-links button").hide();   
@@ -27,9 +21,19 @@ $(".links .texthide").css("display", "flex");
     $("h2").css("font-size", "250%");
     $(".menu > li span, .sub-menu a").css("font-size", "130%");
     $(".menu > li > a").css("font-size", "160%");
-    $("body *").css("font-family","Arial");
+    $("body *").css("font-family","Arial"); 
+};
+
+if (!$.cookie('openspec')) {
+$.cookie('openspec',1, { path: '/' });
+$("body *").css("background", "#fff");
+$("body *").css("color", "#000");
+$("img").css("-webkit-filter", "grayscale(100%)");
+loadform();
+loadstyles();
 }});
 
 if ($.cookie('openspec')===1)
 loadform();
+loadstyles();
 });
