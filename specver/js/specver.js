175591ba79963372955d4508b3505f62fef1b4ef
UserMe <you@example.com>
2019-10-11 13:59:55 +0300

    modified:   specver/js/specver.js

--- specver/js/specver.js
+++ specver/js/specver.js
@@ -3,6 +3,16 @@
 function loadform(){ 
   $( "<button style='float:right'  onclick='$.removeCookie(\"openspec\", { path: \"/\" });location.reload();'>Закрыть версию для слабовидящих</button>" ).insertBefore( "body" );
 $( "body" ).before( "<iframe width='100%' height='110'  style='border: none' src='specver/spec.html'></iframe>" ); 
+};   
+ 
+$('.spec').click(function (e) {
+
+if (!$.cookie('openspec'))
+{
+$.cookie('openspec',1, { path: '/' });
+$("body *").css("background", "#fff");
+$("body *").css("color", "#000");
+$("img").css("-webkit-filter", "grayscale(100%)");
 
 
 let linkimg = document.querySelectorAll(".links img");
@@ -24,22 +34,10 @@ $(".links a .texthide").show();
     $(".menu > li span, .sub-menu a").css("font-size", "130%");
     $(".menu > li > a").css("font-size", "160%");
     $("body *").css("font-family","Arial");
-};   
- 
-$('.spec').click(function (e) {
-
-if (!$.cookie('openspec'))
-{
-$.cookie('openspec',1, { path: '/' });
-$("body *").css("background", "#fff");
-$("body *").css("color", "#000");
-$("img").css("-webkit-filter", "grayscale(100%)");
 loadform();
 }});
 
 if ($.cookie('openspec')==1)
 loadform();
-
-
 
 });
