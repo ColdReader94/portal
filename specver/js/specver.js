fe1b4abf4cabfd8d7fee3789e25d6dad57389ea8
UserMe <you@example.com>
2019-10-11 12:42:05 +0300

    modified:   specver/js/specver.js

--- specver/js/specver.js
+++ specver/js/specver.js
@@ -3,20 +3,10 @@
 function loadform(){ 
   $( "<button style='float:right'  onclick='$.removeCookie(\"openspec\", { path: \"/\" });location.reload();'>Закрыть версию для слабовидящих</button>" ).insertBefore( "body" );
 $( "body" ).before( "<iframe width='100%' height='110'  style='border: none' src='specver/spec.html'></iframe>" ); 
-};   
- 
-$('.spec').click(function (e) {
 
-
-if (!$.cookie('openspec'))
-{
-$.cookie('openspec',1, { path: '/' });
-$("body *").css("background", "#fff");
-$("body *").css("color", "#000");
-$("img").css("-webkit-filter", "grayscale(100%)");
 //Добавлено:
 //    спрятать карусель, изображения блока ссылок, поиск, кнопку спецверсии
-$(".row-two .grid-item-left, .links a img, .search-form, .header-links button").hide();   
+    $(".row-two .grid-item-left, .links a img, .search-form, .header-links button").css("display", "none");   
     
 //    показать ссылки текстом вместо изображений
 $(".links .texthide").css("display", "flex");
@@ -27,6 +17,16 @@ $(".links .texthide").css("display", "flex");
     $(".menu > li span, .sub-menu a").css("font-size", "130%");
     $(".menu > li > a").css("font-size", "160%");
     $("body *").css("font-family","Arial");
+};   
+ 
+$('.spec').click(function (e) {
+
+if (!$.cookie('openspec'))
+{
+$.cookie('openspec',1, { path: '/' });
+$("body *").css("background", "#fff");
+$("body *").css("color", "#000");
+$("img").css("-webkit-filter", "grayscale(100%)");
 loadform();
 }});
 
