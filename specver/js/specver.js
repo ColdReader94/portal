fccfd72a19b86782714f284302b2e3c36df54a1c
UserMe <you@example.com>
2019-10-11 14:10:47 +0300

    Changes to be committed: 	modified:   specver/js/specver.js

--- specver/js/specver.js
+++ specver/js/specver.js
@@ -3,6 +3,27 @@
 function loadform(){ 
   $( "<button style='float:right'  onclick='$.removeCookie(\"openspec\", { path: \"/\" });location.reload();'>Закрыть версию для слабовидящих</button>" ).insertBefore( "body" );
 $( "body" ).before( "<iframe width='100%' height='110'  style='border: none' src='specver/spec.html'></iframe>" ); 
+
+let linkimg = document.querySelectorAll(".links img");
+window.onload = function(){
+for(let e of linkimg){
+    e.style.display = "none";
+   }};
+   
+
+//Добавлено:
+//    спрятать карусель, изображения блока ссылок, поиск, кнопку спецверсии
+$(".row-two .grid-item-left, .links a img, .search-form, .header-links button").hide();   
+    
+//    показать ссылки текстом вместо изображений
+$(".links .texthide").css("display", "flex");
+
+//    увеличенный шрифт по умолчанию
+    $("body p, .header-links, footer a, .navigation a").css("font-size", "150%");
+    $("h2").css("font-size", "250%");
+    $(".menu > li span, .sub-menu a").css("font-size", "130%");
+    $(".menu > li > a").css("font-size", "160%");
+    $("body *").css("font-family","Arial");   
 };   
  
 $('.spec').click(function (e) {
@@ -13,31 +34,10 @@ $.cookie('openspec',1, { path: '/' });
 $("body *").css("background", "#fff");
 $("body *").css("color", "#000");
 $("img").css("-webkit-filter", "grayscale(100%)");
-
-
-let linkimg = document.querySelectorAll(".links img");
-window.onload = function(){
-for(let e of linkimg){
-    e.style.display = "none";
-   }};
-
-//Добавлено:
-//    спрятать карусель, изображения блока ссылок, поиск, кнопку спецверсии
-    $(".row-two .grid-item-left, .search-form, .header-links button").hide();   
-    
-//    показать ссылки текстом вместо изображений
-$(".links a .texthide").show();
-
-//    увеличенный шрифт по умолчанию
-    $("body p, .header-links, footer a, .navigation a").css("font-size", "150%");
-    $("h2").css("font-size", "250%");
-    $(".menu > li span, .sub-menu a").css("font-size", "130%");
-    $(".menu > li > a").css("font-size", "160%");
-    $("body *").css("font-family","Arial");
 loadform();
+
 }});
 
-if ($.cookie('openspec')==1)
+if ($.cookie('openspec')===1)
 loadform();
-
 });
