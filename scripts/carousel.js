987413a3d441a000fca360b421bf23911b15af93
UserMe <you@example.com>
2019-10-23 16:46:55 +0300

    On branch master Changes to be committed: 	modified:   scripts/carousel.js 	modified:   styles/style.css
    	Дополнен скрипт карусели: все закрывается - открывается
    	Добавлен скрипт отображения описания картинки из атрибута alt при открытии картинки. Необходимо дополнить скрипт, чтобы описание выводилось под открывшейся картинкой

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -16,8 +16,8 @@ let carousel = document.querySelector('.carousel');
 //}
 
  
-    let width = 235; // ширина картинки
-    let count = 1; // видимое количество изображений
+    let width = 243; // ширина картинки
+    let count = 1 ; // видимое количество изображений
 
     let list = carousel.querySelector('ul');
     let listElems = carousel.querySelectorAll('li');
@@ -45,21 +45,52 @@ carousel.addEventListener("mousedown", function(event){
   event.preventDefault();
 });
 
-
 //Открытие изображений из карусели 
 document.body.addEventListener('click', function(e){
-
-    
-//  for (let ep of carousel.querySelectorAll('img')){
-//      ep.classList.remove('showimg');};
-  
-//               Если кликнули по картинке
-    if (e.target.matches('li img')) {        
-        return (e.target.classList.toggle('showimg'), undefined);        
-    };
    
-//   клик мимо изображений
-    for (const ep of carousel.querySelectorAll('img'))
-      ep.classList.remove('showimg');
+let clone = e.target.cloneNode(true);
+let parent = e.target.parentElement;
+
+//если фото уже открыто, то при клике на него - закрыть его
+if (e.target.classList.contains('showimg')) {
+    e.target.remove();
+};
+
+//закрыть уже открытое фото если такое есть и перейти на следующее, если нет - открыть новое и вывести описание к нему
+if (e.target.matches('li img')) {
+ for (const ep of carousel.querySelectorAll('.showimg, .lic'))
+       ep.remove();
+       parent.append(clone);
+       clone.license = document.createElement('div');
+       clone.license.innerHTML = e.target.alt;
+//       clone.license.style.margin = "50px";
+//       clone.license.style.background = "#DDBAB8";
+//       clone.license.style.display = "fixed";
+            carousel.prepend(clone.license);
+                    clone.license.classList.add('lic');
       
+       console.log(clone.license);
+     return (clone.classList.add('showimg'));      
+};
+//       Не скрывать фото и описание, если клик по описанию
+         if (e.target === document.body.querySelector('.lic')){
+             return false;
+         }
+       
+//       Скрыть при клике в любом другом месте
+for (const ep of carousel.querySelectorAll(".showimg, .lic"))
+       ep.remove(); 
+ 
   });
+  
+  
+  
+  
+  
+  
+  
+
+
+
+     
+   
